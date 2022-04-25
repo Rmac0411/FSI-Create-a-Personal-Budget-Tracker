b8c54d5b5550055cbf07f12f7fbf26aaa0d1c3e7
rmac0411 <rmac0411@gmail.com>
2022-04-24 22:17:48 -0700

    final chnages

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let numberAnswer = parseFloat(stringAnswer)
     weeklyExpenses = weeklyExpenses + numberAnswer
 }
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    monthlyExpenses = monthlyExpenses + numberAnswer
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    annualExpenses = annualExpenses + numberAnswer
+}
+
